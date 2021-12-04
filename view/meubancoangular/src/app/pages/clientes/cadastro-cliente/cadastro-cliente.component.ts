113cb97c9a59001cdca06f8410229bbc8340a851
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-12-03 21:50:24 -0300

    Validação dos campos do formulário

--- view/meubancoangular/src/app/pages/clientes/cadastro-cliente/cadastro-cliente.component.ts
+++ view/meubancoangular/src/app/pages/clientes/cadastro-cliente/cadastro-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-cliente',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroClienteComponent implements OnInit {
 
+  exform!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.exform = new FormGroup({
+      'name' : new FormControl(null, Validators.required),
+      'cpf' : new FormControl(null, Validators.required),
+      'email' : new FormControl(null, [Validators.required, Validators.email]),
+      'telefone' : new FormControl(null,[
+          Validators.required,
+          Validators.pattern('^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{3})(?: *x(\\d+))?\\s*$')
+        ])
+    });
+  }
+
+  botaoCadastro() {
+    alert('Cadastro efetuado com sucesso!');
+    console.log(this.exform.value);
+    this.exform.reset();
+  }
+
+  get name() {
+    return this.exform.get('name');
+  }
+  get cpf() {
+    return this.exform.get('cpf');
+  }
+  get email() {
+    return this.exform.get('email');
+  }
+  get telefone() {
+    return this.exform.get('telefone');
   }
 
 }
